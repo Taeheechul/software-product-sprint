22940ce9988b749515a526035b30b287747928cc
Jewel Clarke-Henry <jewelclarkehenry@gmail.com>
2020-06-21 23:34:17 -0400

    google maps ?

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,12 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-
 // Carousel
 
 $('.carousel').carousel({
   interval: 2000
 });
+
+/* a map display */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 25});
+}
+
